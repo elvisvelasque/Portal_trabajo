07b204d88632095a316ede33ce6983cd92d87c57
Wilber Javier Ormeño Vera <wilber.ormeno@hotmail.com>
2018-11-25 17:17:30 -0500

    Commit

--- user/jobseeker/jobs.php
+++ user/jobseeker/jobs.php
@@ -1,7 +1,7 @@
 
 <html>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
 <link href="images/hand.png" rel="icon">
 <title>Online job portel</title>
@@ -34,16 +34,10 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 		});
 	});
 </script> 
-<!-- flexslider -->
 <link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" />
-<!-- //flexslider -->
-<!--[if lt IE 9]>
-  <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-<![endif]-->
+<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
 <meta name="viewport" content="width=device-width, initial-scale=1">
- <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
-  --><script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 <style>
 .welcome{
@@ -72,55 +66,62 @@ width:30%;
 							<div class="agileinfo-dot">
 								<div class="container" style="color: #fff;">
                                     <br><br><br><br><br>
-                                    <h2 align="center"> Historial de postulaciones</h2>
+                                    <h2 align="center"> Ofertas laborales</h2>
                                     <br><br>
                                     <div class="container">
-                                        <div class="table-responsive">
-                                            <table class="table table-bordered" style="color: #000; background-color: rgba(255, 255, 255, .5)">
-                                                <tr style="background-color: rgba(211,211,211, .6)">
-                                                    <th style="text-align: center"><h4><b>#</b></h4></th>
-                                                    <th style="text-align: center"><h4><b>Fecha</b></h4></th>
-                                                    <th style="text-align: center"><h4><b>Lugar</b></h4></th>
-                                                    <th style="text-align: center"><h4><b>Puesto</b></h4></th>
-                                                    <th style="text-align: center"><h4><b>Estado</b></h4></th>
-                                                </tr>
-                                                <tr>
-                                                    <td align="center">1</td>
-                                                    <td align="center">12/11/2018</td>
-                                                    <td align="center">Lima</td>
-                                                    <td align="center">Practicante sistemas</td>
-                                                    <td align="center">Postulación</td>
-                                                </tr>
-                                                <tr>
-                                                    <td align="center">2</td>
-                                                    <td align="center">12/11/2018</td>
-                                                    <td align="center">Lima</td>
-                                                    <td align="center">Teleoperadora</td>
-                                                    <td align="center">Examen Psicológico</td>
-                                                </tr>
-                                                <tr>
-                                                    <td align="center">3</td>
-                                                    <td align="center">12/11/2018</td>
-                                                    <td align="center">Lima</td>
-                                                    <td align="center">Marketing y Diseño</td>
-                                                    <td align="center">Examen Psicológico</td>
-                                                </tr>
-                                                <tr>
-                                                    <td align="center">4</td>
-                                                    <td align="center">12/11/2018</td>
-                                                    <td align="center">Lima</td>
-                                                    <td align="center">Examen Conocimientos</td>
-                                                    <td align="center">Postulación</td>
-                                                </tr>
-                                                <tr>
-                                                    <td align="center">5</td>
-                                                    <td align="center">12/11/2018</td>
-                                                    <td align="center">Lima</td>
-                                                    <td align="center">Asistente</td>
-                                                    <td align="center">Postulación</td>
-                                                </tr>
+                                        <div class="col-md-3" style="background-color: #24246A; padding: 30px 12px" >
+                                            <input type="text" class="form-control" placeholder="Palabra clave">
+                                            <br>
+                                            <button type="button" class="btn btn-info">Búsqueda</button>
+                                        </div>
+                                        <div class="col-lg-9">
+                                            <div class="table-responsive">
+                                                <table class="table table-bordered" style="color: #000; background-color: rgba(255, 255, 255, .5)">
+                                                    <tr style="background-color: rgba(211,211,211, .6)">
+                                                        <th style="text-align: center"><h4><b>#</b></h4></th>
+                                                        <th style="text-align: center"><h4><b>Fecha</b></h4></th>
+                                                        <th style="text-align: center"><h4><b>Lugar</b></h4></th>
+                                                        <th style="text-align: center"><h4><b>Puesto</b></h4></th>
+                                                        <th style="text-align: center"><h4><b>Estado</b></h4></th>
+                                                    </tr>
+                                                    <tr>
+                                                        <td align="center">1</td>
+                                                        <td align="center">12/11/2018</td>
+                                                        <td align="center">Lima</td>
+                                                        <td align="center">Practicante sistemas</td>
+                                                        <td align="center">Postulación</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td align="center">2</td>
+                                                        <td align="center">12/11/2018</td>
+                                                        <td align="center">Lima</td>
+                                                        <td align="center">Teleoperadora</td>
+                                                        <td align="center">Examen Psicológico</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td align="center">3</td>
+                                                        <td align="center">12/11/2018</td>
+                                                        <td align="center">Lima</td>
+                                                        <td align="center">Marketing y Diseño</td>
+                                                        <td align="center">Examen Psicológico</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td align="center">4</td>
+                                                        <td align="center">12/11/2018</td>
+                                                        <td align="center">Lima</td>
+                                                        <td align="center">Examen Conocimientos</td>
+                                                        <td align="center">Postulación</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td align="center">5</td>
+                                                        <td align="center">12/11/2018</td>
+                                                        <td align="center">Lima</td>
+                                                        <td align="center">Asistente</td>
+                                                        <td align="center">Postulación</td>
+                                                    </tr>
 
-                                            </table>
+                                                </table>
+                                            </div>
                                         </div>
                                     </div>
                                     <br><br><br>
@@ -157,9 +158,9 @@ width:30%;
 						<nav>
 							<ul class="nav navbar-nav navbar-right">
 								<li><a href="#">Inicio</a></li>
-								<li><a href="jobs.php">Puestos laborales</a></li>
+								<li class="active"><a href="jobs.php">Puestos laborales</a></li>
                                 <li><a href="my_cv.php">Mi CV</a></li>
-                                <li class="active"><a href="index.php">Mis postulaciones</a></li>
+                                <li><a href="index.php">Mis postulaciones</a></li>
 							    <li><a href="../../logout.php" >Cerrar sesión</a></li>
                                                                 
 							</ul>
---
+++ user/jobseeker/my_cv.php
@@ -0,0 +1,224 @@
+
+<html>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<link href="images/hand.png" rel="icon">
+<title>Online job portel</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset="utf-8">
+<meta name="keywords" content="Bank Online Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- bootstrap-css -->
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<!--// bootstrap-css -->
+<!-- css -->
+<link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+<!--// css -->
+<!-- font-awesome icons -->
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<!-- //font-awesome icons -->
+<!-- font -->
+<link href="//fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Crimson+Text:400,400i,600,600i,700,700i" rel="stylesheet">
+<link href='//fonts.googleapis.com/css?family=Roboto+Condensed:400,700italic,700,400italic,300italic,300' rel='stylesheet' type='text/css'>
+<!-- //font -->
+<script src="js/jquery-1.11.1.min.js"></script>
+<script src="js/bootstrap.js"></script>
+<script type="text/javascript">
+	jQuery(document).ready(function($) {
+		$(".scroll").click(function(event){		
+			event.preventDefault();
+			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+		});
+	});
+</script> 
+<link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" />
+<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<style>
+.welcome{
+text-align:center;
+float:left;
+margin-left:40px;
+margin-top:-20px;
+color:#e5f29d;
+width:30%;
+}
+.copyright p {
+    text-align: left;
+    color: #FFFFFF;
+    margin: 0;
+    font-size: .9em;
+}
+</style>
+</head>
+<body>
+
+	<div class="banner-top">
+		<div class="slider">
+			<div class="callbacks_container">
+				<ul class="rslides callbacks callbacks1" id="slider4">
+					<li>
+						<div class="w3layouts-banner-top jarallax">
+							<div class="agileinfo-dot">
+                                <div class="container" style="color: #fff;">
+                                    <br><br><br><br><br><br>
+                                    <div class="container">
+                                        <div class="col-md-3">
+                                            <img src="images/user.png" alt="" style="width: 100%">
+                                        </div>
+                                        <div class="col-lg-9">
+                                            <div class="container-fluid" style="background-color: whitesmoke; color: #000">
+                                                <h3 style="border-bottom: 2px solid red">Datos personales</h3>
+                                                <div class="col-md-12">
+                                                    <p>Nombres y apellidos</p>
+                                                    <p style="margin-top: -12px"><b>Melissa Calero Ortiz</b></p>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <p>DNI</p>
+                                                    <p style="margin-top: -12px"><b>76773668</b></p>
+                                                    <p>Dirección</p>
+                                                    <p style="margin-top: -12px"><b>Jirón Ica 329</b></p>
+                                                    <p>Distrito</p>
+                                                    <p style="margin-top: -12px"><b>San Juan de Lurigancho</b></p>
+                                                    <p>Correo Electrónico</p>
+                                                    <p style="margin-top: -12px"><b>melissacaleroortiz@gmail.com</b></p>
+                                                    <p>Celular/Teléfono</p>
+                                                    <p style="margin-top: -12px"><b>0122775548</b></p>
+
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <p>Edad</p>
+                                                    <p style="margin-top: -12px"><b>23</b></p>
+                                                    <p>Género</p>
+                                                    <p style="margin-top: -12px"><b>Femenino</b></p>
+                                                    <p>Estado Civil</p>
+                                                    <p style="margin-top: -12px"><b>Sotera</b></p>
+                                                    <p>Lugar de Nacimiento</p>
+                                                    <p style="margin-top: -12px"><b>Lima</b></p>
+                                                </div>
+                                            </div>
+                                            <br>
+                                            <div class="container-fluid" style="background-color: whitesmoke; color: #000">
+                                                <h3 style="border-bottom: 2px solid red">Estudios</h3>
+                                                <div class="col-md-12">
+                                                    <p>Institución</p>
+                                                    <p style="margin-top: -12px"><b>Universidad César Vallejo</b></p>
+                                                    <p>Carrera</p>
+                                                    <p style="margin-top: -12px"><b>Ingeniería de Sistemas</b></p>
+                                                </div>
+                                            </div>
+                                            <br>
+                                            <div class="container-fluid" style="background-color: whitesmoke; color: #000">
+                                                <h3 style="border-bottom: 2px solid red">Aptitudes</h3>
+                                                <div class="col-md-12">
+                                                    <p><b>Liderazgo</b></p>
+                                                    <p style="margin-top: -12px"><b>Compañerismo</b></p>
+                                                    <p style="margin-top: -12px"><b>Trabajo en equipo</b></p>
+                                                    <p style="margin-top: -12px"><b>Comunicación</b></p>
+                                                </div>
+                                            </div>
+                                            <br>
+                                            <div class="container-fluid" style="background-color: whitesmoke; color: #000">
+                                                <h3 style="border-bottom: 2px solid red">Conocimientos</h3>
+                                                <div class="col-md-12">
+                                                    <p><b>Análisis de sistemas</b></p>
+                                                    <p style="margin-top: -12px"><b>PHP</b></p>
+                                                    <p style="margin-top: -12px"><b>JAVA</b></p>
+                                                    <p style="margin-top: -12px"><b>RUP</b></p>
+                                                </div>
+                                            </div>
+                                            <br>
+                                            <div class="container-fluid" style="background-color: whitesmoke; color: #000">
+                                                <h3 style="border-bottom: 2px solid red">Experiencia Laboral</h3>
+                                                <div class="col-md-12">
+                                                    <p>Puesto</p>
+                                                    <p style="margin-top: -12px"><b>Practicante</b></p>
+                                                    <p>Descripción</p>
+                                                    <p style="margin-top: -12px"><b>Apoyo Soporte</b></p>
+                                                </div>
+                                            </div>
+                                            <br>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+						</div>
+					</li>
+				</ul>
+			</div>
+			<div class="clearfix"> </div>
+			<script src="js/responsiveslides.min.js"></script>
+		</div>
+	</div>
+	<!-- banner -->
+	<div class="banner">
+		<div class="header">
+			<div class="container">
+				<nav class="navbar navbar-default">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+					  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					  </button>
+
+                        <div class="w3layouts-logo">
+                            <h1><a href="index.html"> <span>Online</span>Job<span>portal</span></a></h1>
+                        </div>
+                    </div>
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse nav-wil" id="bs-example-navbar-collapse-1">
+						<nav>
+							<ul class="nav navbar-nav navbar-right">
+								<li><a href="#">Inicio</a></li>
+								<li><a href="jobs.php">Puestos laborales</a></li>
+                                <li class="active"><a href="my_cv.php">Mi CV</a></li>
+                                <li><a href="index.php">Mis postulaciones</a></li>
+							    <li><a href="../../logout.php" >Cerrar sesión</a></li>
+                                                                
+							</ul>
+						</nav>
+					</div>
+					<!-- /.navbar-collapse -->
+				</nav>
+			</div>
+		</div>
+	</div>
+    <footer>
+        <div class="container">
+            <div class="copyright">
+                <div class="row">
+                    <div class="col-sm-2"></div>
+                    <div class="col-sm-3" style="align-items: left">
+                        <p align="left"><b><a href="default.asp" target="_blank">Inicio</a></b></p>
+                        <p><b><a href="default.asp" target="_blank">Puestos Laborales</a></b></p>
+                        <p><b><a href="default.asp" target="_blank">Mi CV</a></b></p>
+                        <p><b><a href="default.asp" target="_blank">Postulaciones</a></b></p>
+                    </div>
+                    <div class="col-sm-4 col-sm-offset-2">
+                        <label style="color: #fff;">Suscribete a nuestro boletín!</label>
+                        <div class="col-sm-7">
+                            <input type="text" class="form-control" placeholder="Ingresa tu email" style="width: auto">
+                        </div>
+                        <div class="col-sm-5" style="align-items: left">
+                            <button type="button" class="btn btn-info">Suscribir</button>
+                        </div>
+                    </div>
+
+                </div>
+                <br>
+                <br>
+            </div>
+            <p align="center" style="color: #fff">© 2018 Multitron-Bolsa de trabajo . Todos los derechos reservados <a href="#"></a></p>
+
+        </div>
+    </footer>
+</body>
+</html>
