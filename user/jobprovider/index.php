e182c6eab9806e118e7386d26c680673f4fe817d
Wilber Javier Ormeño Vera <wilber.ormeno@hotmail.com>
2018-12-16 04:17:46 -0500

    update cv

--- user/jobprovider/index.php
+++ user/jobprovider/index.php
@@ -48,6 +48,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
  <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
   --><script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
 </head>
 <?php
     include("../../connection.php");
